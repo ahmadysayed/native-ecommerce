075f92adbd69074e90ffa212eedec9ee1585a475
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2024-11-09 20:33:06 +0100

    [UI] learn about flex direction

--- App.js
+++ App.js
@@ -2,8 +2,18 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Hello React Nativ </Text>
+    <View
+      style={{
+        flexDirection: "row",
+        flex: 0.5,
+        alignItems: "flex-end",
+        justifyContent: "flex-end",
+        backgroundColor: "#bbb",
+      }}
+    >
+      <Text style={{ fontSize: 20 }}>Hello </Text>
+      <Text style={{ fontSize: 22 }}>Let's program in</Text>
+      <Text style={{ fontSize: 25 }}>React Native </Text>
     </View>
   );
 }
