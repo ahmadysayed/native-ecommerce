80e042308a7b08871b173f4315cebf864b794d8b
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2024-11-09 20:52:45 +0100

    [UI] showing text input and opactity event

--- src/components/WelcomeView.js
+++ src/components/WelcomeView.js
@@ -1,11 +1,34 @@
-import { View, Text, StyleSheet } from "react-native";
+import { Feather, Ionicons } from "@expo/vector-icons";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
 function WelcomeView() {
   return (
-    <View>
-      <Text style={styles.welcomeTxt("#000", 70)}>Find The Most</Text>
+    <View style={{}}>
+      <View>
+        <Text style={styles.welcomeTxt("#000", 70)}>Find The Most</Text>
 
-      <Text style={styles.welcomeTxt("#2A4D50", 0)}>Luxurious Furniture</Text>
+        <Text style={styles.welcomeTxt("#2A4D50", 0)}>Luxurious Furniture</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <TouchableOpacity
+          onPress={() => {
+            console.log("I am tapped");
+          }}
+        >
+          <Feather name="search" size={24} style={styles.searchIcon} />
+        </TouchableOpacity>
+
+        <TextInput
+          placeholder="What are you looking for"
+          style={styles.searchInput}
+        />
+      </View>
     </View>
   );
 }
@@ -15,7 +38,23 @@ const styles = StyleSheet.create({
     fontSize: 38,
     color: color,
     marginTop: top,
+    marginHorizontal: 12,
   }),
+
+  searchIcon: {
+    marginTop: 12,
+    color: "#83829A",
+    marginLeft: 10,
+  },
+  searchContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    height: 50,
+    marginHorizontal: 16,
+  },
+  searchInput: {
+    width: "100%",
+  },
 });
 
 export default WelcomeView;
