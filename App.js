719c0890a3e0280265ec64d786c6e497de667f44
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2024-11-09 20:36:52 +0100

    [UI] seperated styles

--- App.js
+++ App.js
@@ -1,19 +1,45 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        flex: 0.5,
-        alignItems: "flex-end",
-        justifyContent: "flex-end",
-        backgroundColor: "#bbb",
-      }}
-    >
-      <Text style={{ fontSize: 20 }}>Hello </Text>
-      <Text style={{ fontSize: 22 }}>Let's program in</Text>
-      <Text style={{ fontSize: 25 }}>React Native </Text>
+    <View style={styles.container}>
+      <View style={styles.topBox}>
+        <Text style={{ fontSize: 20 }}>Hello</Text>
+        <Text style={{ fontSize: 22 }}>Let's program in</Text>
+        <Text style={{ fontSize: 25 }}>React Native</Text>
+      </View>
+
+      <View style={styles.bottomBox}>
+        <Text style={{ fontSize: 20 }}>Hello</Text>
+        <Text style={{ fontSize: 22 }}>Let's program in</Text>
+        <Text style={{ fontSize: 25 }}>React Native</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#bbb",
+  },
+
+  topBox: {
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "colum",
+    flex: 0.3,
+    backgroundColor: "#ccc",
+  },
+
+  bottomBox: {
+    flexDirection: "column",
+    flex: 0.3,
+    backgroundColor: "#ddd",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
