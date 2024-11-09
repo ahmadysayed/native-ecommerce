36c72cc930fd41b8ff97c80fbf53675b39b57063
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2024-11-09 21:01:37 +0100

    [UI] optimization

--- App.js
+++ App.js
@@ -1,37 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native";
 import WelcomeView from "./src/components/WelcomeView";
-
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
       <WelcomeView />
-
-      <View style={styles.box}>
-        <Text style={{ fontSize: 20 }}>Hello</Text>
-        <Text style={{ fontSize: 22 }}>Let's program in</Text>
-        <Text style={{ fontSize: 25 }}>React Native</Text>
-      </View>
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "column",
-    flex: 1,
-    alignItems: "center",
-
-    backgroundColor: "#bbb",
-  },
-
-  box: {
-    flexDirection: "column",
-
-    backgroundColor: "#ddd",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10,
-    marginTop: 10,
-    padding: 40,
-  },
-});
--- src/components/WelcomeView.js
+++ src/components/WelcomeView.js
@@ -6,14 +6,19 @@ import {
   TouchableOpacity,
   TextInput,
 } from "react-native";
+import { theme } from "../constants/theme";
 
 function WelcomeView() {
   return (
     <View style={{}}>
       <View>
-        <Text style={styles.welcomeTxt("#000", 70)}>Find The Most</Text>
+        <Text style={styles.welcomeTxt(theme.colors.black, 70)}>
+          Find The Most
+        </Text>
 
-        <Text style={styles.welcomeTxt("#2A4D50", 0)}>Luxurious Furniture</Text>
+        <Text style={styles.welcomeTxt(theme.colors.primary, 0)}>
+          Luxurious Furniture
+        </Text>
       </View>
       <View style={styles.searchContainer}>
         <TouchableOpacity
@@ -24,36 +29,63 @@ function WelcomeView() {
           <Feather name="search" size={24} style={styles.searchIcon} />
         </TouchableOpacity>
 
-        <TextInput
-          placeholder="What are you looking for"
-          style={styles.searchInput}
-        />
+        <View style={styles.searchWrapper}>
+          <TextInput
+            placeholder="What are you looking for"
+            style={styles.searchInput}
+          />
+        </View>
+        <View style={styles.searchBtn}>
+          <Ionicons
+            name="camera-outline"
+            size={36}
+            color={theme.colors.offWhite}
+          />
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  searchWrapper: {
+    flex: 1,
+    backgroundColor: theme.colors.secondary,
+    borderRadius: theme.sizes.small,
+    marginRight: theme.sizes.small,
+  },
+  searchBtn: {
+    width: 50,
+    backgroundColor: theme.colors.primary,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: theme.sizes.medium,
+  },
   welcomeTxt: (color, top) => ({
-    fontSize: 38,
+    fontSize: theme.sizes.xxlarge - 6,
     color: color,
     marginTop: top,
-    marginHorizontal: 12,
+    marginHorizontal: theme.sizes.small,
   }),
 
   searchIcon: {
-    marginTop: 12,
-    color: "#83829A",
-    marginLeft: 10,
+    marginTop: theme.sizes.small,
+    color: theme.colors.gray,
+    marginHorizontal: theme.sizes.xSmall,
   },
   searchContainer: {
     flexDirection: "row",
     justifyContent: "center",
     height: 50,
-    marginHorizontal: 16,
+    marginHorizontal: theme.sizes.small,
+    backgroundColor: theme.colors.secondary,
+    borderRadius: theme.sizes.medium,
+    marginVertical: theme.sizes.medium,
   },
   searchInput: {
     width: "100%",
+    height: "100%",
+    paddingHorizontal: theme.sizes.small,
   },
 });
 
