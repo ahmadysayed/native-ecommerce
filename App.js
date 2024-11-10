c840c9e3af82154f82f1caf19551e06c5d2bf070
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2024-11-10 19:08:56 +0100

    [UI] arrow function

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { SafeAreaView } from "react-native";
 import WelcomeComponent from "./src/components/WelcomeComponent";
 import HomeCarouselComponent from "./src/components/HomeCarouselComponent";
 
-
 export default function App() {
   return (
     <SafeAreaView>
