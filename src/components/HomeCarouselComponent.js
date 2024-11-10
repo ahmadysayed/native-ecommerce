14e4af9874fe37f2e1fef2b2fec386af20413272
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2024-11-10 19:23:47 +0100

    [UI] flatlist and dynamic data

--- src/components/HomeCarouselComponent.js
+++ src/components/HomeCarouselComponent.js
@@ -2,40 +2,40 @@ import { Text, Image, View, FlatList } from "react-native";
 import { theme } from "../constants/theme";
 
 const data = [
-    {id:'1', title:'Item 1'},
-    {id:'2', title:'Item 2'},
-    {id:'3', title:'Item 3'},
-    {id:'4', title:'Item 4'},
-    {id:'5', title:'Item 5'},
+  { id: "1", title: "Item 1" },
+  { id: "2", title: "Item 2" },
+  { id: "3", title: "Item 3" },
+  { id: "4", title: "Item 4" },
+  { id: "5", title: "Item 5" },
 ];
 
 const HomeCarouselComponent = () => {
   return (
-   <View>
-     <View
-      style={{
-        height: 200,
-        backgroundColor: theme.colors.offWhite,
-        marginHorizontal: theme.sizes.medium,
-        borderRadius: theme.sizes.medium,
-        overflow:"hidden",
-        justifyContent:"center",
-        alignItems: "center"
-      }}
-    >
-      <Image
-        style={{ resizeMode: "cover", width: "100%", height: "100%" }}
-        source={require("../../assets/images/fn1.jpg")}
-      />
-    </View>
     <View>
-        <FlatList 
-            data={data}
-            renderItem={()=><Text>Hello</Text>}
-           
+      <View
+        style={{
+          height: 200,
+          backgroundColor: theme.colors.offWhite,
+          marginHorizontal: theme.sizes.medium,
+          borderRadius: theme.sizes.medium,
+          overflow: "hidden",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          style={{ resizeMode: "cover", width: "100%", height: "100%" }}
+          source={require("../../assets/images/fn1.jpg")}
         />
+      </View>
+      <View>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => <Text>{item.title}</Text>}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
-   </View>
   );
 };
 
